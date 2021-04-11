f69a7292f018612311e41885225173c939d60064
mmpacker <mmpacker@gmail.com>
2021-04-11 13:51:31 -0400

    Colpleted challenge 15.

--- challenges.js
+++ challenges.js
@@ -456,7 +456,9 @@ mergeObjects({a: 1, b: 2, c: 3}, {d: 4}, {b: 22, d: 44}); //=> {a: 1, b: 22, c: 
 -----------------------------------------------------------------------------*/
 // Your solution for 15-mergeObjects here:
 
-
+function mergeObjects(target, ...source) {
+  return Object.assign(target, ...source);
+}
 
 
 
