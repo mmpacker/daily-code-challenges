a2655bb6f18d235316bc6a7e0f382549d9760495
mmpacker <mmpacker@gmail.com>
2021-05-10 10:45:16 -0400

    solve challenge 26

--- challenges.js
+++ challenges.js
@@ -889,8 +889,12 @@ toCamelCase( 'A_b_c' ) // => 'ABC'
 -----------------------------------------------------------------------------*/
 // Your solution for 26-toCamelCase here:
 
-
-
+function toCamelCase(str) {
+  const regExp = /[-_]\w/ig
+  return str.replace(regExp,(match) => {
+    return match[1].toUpperCase()
+  })
+}
 
 
 /*-----------------------------------------------------------------------------
